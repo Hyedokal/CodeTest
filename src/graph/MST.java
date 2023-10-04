3781a5b1315ac583b0036e66c35035776c1a1ea1
Hyedokal <xcvbn5414@gmail.com>
2023-10-04 21:35:20 +0900

    10/04 - 2

---
+++ src/graph/MST.java
@@ -0,0 +1,45 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/* 백준 1197(골드 4)
+* 그래프가 주어졌을 때, 그 그래프의 최소 스패닝 트리를 구하는 프로그램을 작성하시오.
+* 최소 스패닝 트리는, 주어진 그래프의 모든 정점들을 연결하는 부분 그래프 중에서
+* 그 가중치의 합이 최소인 트리를 말한다.
+*첫째 줄에 정점의 개수 V(1 ≤ V ≤ 10,000)와 간선의 개수 E(1 ≤ E ≤ 100,000)가 주어진다.
+*  다음 E개의 줄에는 각 간선에 대한 정보를 나타내는 세 정수 A, B, C가 주어진다.
+*  이는 A번 정점과 B번 정점이 가중치 C인 간선으로 연결되어 있다는 의미이다. C는 음수일 수도 있으며, 절댓값이 1,000,000을 넘지 않는다.
+* 그래프의 정점은 1번부터 V번까지 번호가 매겨져 있고, 임의의 두 정점 사이에 경로가 있다.
+* 최소 스패닝 트리의 가중치가 -2,147,483,648보다 크거나 같고, 2,147,483,647보다 작거나 같은 데이터만 입력으로 주어진다.
+*/
+public class MST {
+    static int[] parent; //대표 노드 배열
+    static int V; //노드 개수
+    static int E; //에지 개수
+    static PriorityQueue<> PQ; //우선순위 큐
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        parent = new int[V+1];
+        PQ = new PriorityQueue();   //우선순위 큐
+        for(int i=0; i<V+1; i++){
+            parent[i] = i;  //대표노드 배열 초기화
+        }
+
+        for(int i=0; i<E; i++){
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            //PQ.offer(C, A, B);
+        }
+    }
+
+
+}
